65c967b3940584a8bc0f6ee00696737f1d63ee56
Infiinite-lens <okorelens27@gmail.com>
2022-07-18 16:09:57 +0300

    Added routing to the app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule, routingComponents } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./src/components/navbar/navbar.component";
-import { HomeComponent } from "./src/components/pages/home/home.component";
-import { CardComponent } from './src/ui/cards/card/card.component';
-import { FooterComponent } from './src/components/footer/footer.component';
+import { CardComponent } from "./src/ui/cards/card/card.component";
+import { FooterComponent } from "./src/components/footer/footer.component";
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, CardComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    FooterComponent,
+    routingComponents,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
