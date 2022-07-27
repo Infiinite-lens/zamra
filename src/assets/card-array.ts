93cf958894922dfcd3f149c74026e7c5183c4ddf
Infiinite-lens <okorelens27@gmail.com>
2022-07-27 11:26:28 +0300

    Changes

---
+++ src/assets/card-array.ts
@@ -0,0 +1,45 @@
+interface CardElement {
+  title: string;
+  text: string;
+}
+
+const cardArr: CardElement[] = [
+  {
+    title: "Title 1",
+    text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
+  },
+  {
+    title: "Title 2",
+    text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
+  },
+  {
+    title: "Title 3",
+    text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
+  },
+  {
+    title: "Title 4",
+    text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
+  },
+  {
+    title: "Title 5",
+    text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
+  },
+  {
+    title: "Title 6",
+    text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
+  },
+  {
+    title: "Title 7",
+    text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
+  },
+  {
+    title: "Title 8",
+    text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
+  },
+  {
+    title: "Title 9",
+    text: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
+  },
+];
+
+export default cardArr;
